e27b93fe55080c1dbe5592ffb898b431deeaf087
William Gérald Blondel <contact@williamblondel.fr>
2023-02-18 03:18:12 +0800

    Created Users and Teams pages

---
+++ app/Filament/Resources/TeamResource/RelationManagers/UsersRelationManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\TeamResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UsersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'users';
+
+    protected static ?string $recordTitleAttribute = 'email';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable(),
+                Tables\Columns\TextColumn::make('email')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\AssociateAction::make(),
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\DissociateAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DissociateBulkAction::make(),
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+}
